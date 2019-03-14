8a3ab56b74f82b17180397148407a98fea618115
Fernando <fernando_lopezc@live.com>
2019-03-14 08:01:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+nu_of_setence = self.split(/[.?!]/).reject {|string| string.empty?}.length
   end
 end
+
+# def sentence?(string)
+#   if string[string.length-1] == "."
+#     return true
+#   end
+#   return false 
+# end
+# binding.pry
+# sentence?("Hi, my name is Sophie.")
+
+#str.sentence?
